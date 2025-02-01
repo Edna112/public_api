0c193c58ff6c6791f87d3b4b75b91553c0e08b54
Edna112 <chenwieddy@gmail.com>
2025-02-01 08:16:18 -0800

    initiall commit for vercel deployment application

--- vercel-functions/info.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-header('Content-type: application.json');
-
-echo json_encode([
-    "email" => "chenwieddy@gmail.com",
-    "current_datetime" => define('DATE_ISO8601', "Y-m-d\TH:i:sO"),
-    "github_url" => "https://github.com/Edna112/public_api",
-]);
